afa226f73c5177342e13e6ae8ac6a93e6f633ad3
avirupdutta <finalguider99@gmail.com>
2020-02-24 21:50:59 +0530

    added files

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// var x;
+// x = 1000;
+// console.log("x = " + x);
+// var x = "Hi";
+// console.log("x = " + x);
+
+1, 2, 3, 4
+1.5, 51.3698752
+var x = "This is javascript";
+console.log(x);
+
+
+
