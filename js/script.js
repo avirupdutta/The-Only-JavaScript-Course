b3b83817dfcd4f09a0d95db55f64efe8690fe987
avirupdutta <finalguider99@gmail.com>
2020-04-10 22:54:07 +0530

    control flow part 2

--- js/script.js
+++ js/script.js
@@ -5,24 +5,8 @@
  * 3) Ternary operator
  */
 
-// if (age >= 18) {
-// 	var name = "Avirup ";
-// 	console.log(name + age);
-// }
+var age = 9;
 
-// if (age >= 18 && age <= 25) {
-// 	console.log(age);
-// }
-
-// if (age <= 18 || age >= 25) {
-// 	console.log(age);
-// }
-
-var age = 10;
-if (age >= 60) {
-	console.log("You are a senior citizen");
-} else if (age >= 18) {
-	console.log("You are an adult");
-} else {
-	console.log("You are not an adult");
-}
+console.log(age >= 18 ? (
+	age <= 10 ? 'age is less than 10': 'age is more than 10'
+	) : "You are not an adult");
